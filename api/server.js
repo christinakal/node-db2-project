e8897fd2ff7eec3d2429ecff4cc8814fb09db17a
christina kaliora <develop@pirron.one>
2020-02-19 00:57:55 +0100

    IT'S WORKIIIIIIINGgit add .!

--- api/server.js
+++ api/server.js
@@ -1,3 +1,10 @@
-const express = require('express');
+const express = require("express");
 const server = express();
 
+const carsRouter = require("../cars/cars-router.js");
+
+server.use(express.json());
+
+server.use("/api/cars", carsRouter);
+
+module.exports = server;
