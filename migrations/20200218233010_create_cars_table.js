26160154b95638846bc68ba9d7f879c5ecc4bd21
christina kaliora <develop@pirron.one>
2020-02-19 00:22:00 +0100

    small additions

--- migrations/20200218233010_create_cars_table.js
+++ migrations/20200218233010_create_cars_table.js
@@ -4,20 +4,20 @@ exports.up = function(knex) {
   return knex.schema.createTable("cars", tbl => {
     //adds an id column that auto increments
     tbl.increments();
-    tbl.integer("VIN");
+    tbl.integer("VIN").notNullable();
 
     tbl
       .string("make", 50)
       .notNullable()
       .index();
 
-    tbl.string("model", 50).notNullable();
+    tbl.string("model", 50);
 
     tbl.float("mileage", 300);
 
-    tbl.string("transmission", 250);
+    tbl.string("transmission", 250).nullable();
 
-    tbl.string("status", 250);
+    tbl.string("status", 250).nullable();
   });
 };
 
